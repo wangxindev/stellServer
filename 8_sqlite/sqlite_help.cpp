8c6c69c5de7dcaddf5c5cb1e54aa1630310dd226
wangxin <wangxindev@126.com>
2017-05-23 09:29:37 +0800

    sqlite锁更改，之前的锁有问题

--- 8_sqlite/sqlite_help.cpp
+++ 8_sqlite/sqlite_help.cpp
@@ -4,44 +4,40 @@
 #include <cstdio>
 #include <mutex>
 
-class mutexHelp
-{
-public:
-	mutexHelp() { mtx.lock(); }
-	~mutexHelp() { mtx.unlock(); }
-	std::mutex mtx;
-};
+std::mutex sqlite_mtx;
 
 int sqlite_help_one::createTable(char * dataBase, string sql, char **errmsg)
 {
-	mutexHelp mtx;
+	sqlite_mtx.lock();
 	sqlite3 *db;
 	int rc = runSql(dataBase, sql, NULL, NULL, errmsg);
 	printErrInfo(rc, errmsg);
+	sqlite_mtx.unlock();
 	return rc;
 }
 
 int sqlite_help_one::insertTable(char * dataBase, string sql, char **errmsg)
 {
-	mutexHelp mtx;
+	sqlite_mtx.lock();
 	sqlite3 *db;
 	int rc = runSql(dataBase, sql, NULL, NULL, errmsg);
 	printErrInfo(rc, errmsg);
+	sqlite_mtx.unlock();
 	return rc;
 }
 
 int sqlite_help_one::selectTable(char * dataBase, string sql, int(*callback)(void*, int, char**, char**), char **errmsg, void * param)
 {
-	mutexHelp mtx;
+	sqlite_mtx.lock();
 	sqlite3 *db;
 	int rc = runSql(dataBase, sql, callback, param, errmsg);
 	printErrInfo(rc, errmsg);
+	sqlite_mtx.unlock();
 	return rc;
 }
 
 int sqlite_help_one::runSql(const char *database, string sql, int(*callback)(void*, int, char**, char**), void * v, char **errmsg)
 {
-	mutexHelp mtx;
 	sqlite3 *db;
 	int rc = sqlite3_open(database, &db);
 	if (rc == SQLITE_OK)
@@ -59,7 +55,6 @@ int sqlite_help_one::runSql(const char *database, string sql, int(*callback)(voi
 
 int sqlite_help_one::printErrInfo(int rc, char **errinfo)
 {
-	mutexHelp mtx;
 	if (rc > -1)
 	{
 		if (rc != SQLITE_OK)
